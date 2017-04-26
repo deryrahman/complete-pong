052d8587ea5327cd381616e39f2a7e3fa789780b
faizghifari <fgh1997@live.com>
2017-04-26 09:57:22 +0700

    add javadoc for centerboard

--- src/centerboard/CenterArea.java
+++ src/centerboard/CenterArea.java
@@ -3,17 +3,44 @@ package centerboard;
 import java.util.Random;
 
 /**
- * Created by Faiz Ghifari Haznitrama on 22/04/2017.
+ * Center Area Class as model class to provide area
+ * for centerboard objects to spawn itself
+ * @author Dery Rahman Ahaddienata <13515097@std.stei.itb.ac.id>
+ * @version 1.0
+ * @since 1.0
  */
 
 public class CenterArea {
+    /**
+     * define all places provided
+     */
     protected static Cell[][] cell;
 
+    /**
+     * define vertical size of center area
+     */
     protected int height;
+
+    /**
+     * define horizontal size of center area
+     */
     protected int width;
+
+    /**
+     * define vertical zone provided by cell
+     */
     protected int matrixHeight=10;
+
+    /**
+     * define horizontal zone provided by cell
+     */
     protected int matrixWidth=10;
 
+    /**
+     * CenterArea constructor, initialize area
+     * @param width = horizontal area
+     * @param height = vertical area
+     */
     public CenterArea(int width, int height) {
         this.width=width;
         this.height=height;
@@ -26,9 +53,30 @@ public class CenterArea {
         }
     }
 
+    /**
+     * get area's height
+     * @return area's height
+     */
     public float getHeight() { return height; }
+
+    /**
+     * get area's width
+     * @return area's width
+     */
     public float getWidth() { return width; }
+
+    /**
+     * get object on place (i,j)
+     * @param i = horizontal
+     * @param j = vertical
+     * @return object cell
+     */
     public Cell getCell(int i, int j) { return cell[i][j]; }
+
+    /**
+     * spawning random object on random place on center area
+     * @param type = parameter for object spawned
+     */
     public void setRandomCell(int type) {
         int i, j;
         Random rand = new Random();
@@ -45,6 +93,12 @@ public class CenterArea {
             cell[i][j].spawn();
         }
     }
+
+    /**
+     * destroy object on cell
+     * @param i = horizontal
+     * @param j = vertical
+     */
     public void destroyCell(int i, int j) { cell[i][j] = null; }
 
 }
--- src/centerboard/PowerUp.java
+++ src/centerboard/PowerUp.java
@@ -4,9 +4,18 @@ import models.Ball;
 import models.Paddle;
 
 /**
- * Created by Faiz Ghifari Haznitrama on 22/04/2017.
+ * Interface for power up. All power ups must implement
+ * all methods provided here
+ * @author Faiz Ghifari Haznitrama <13515010@std.stei.itb.ac.id>
+ * @version 1.0
+ * @since 1.0
  */
 
 public interface PowerUp {
+    /**
+     * Use power up based on the owner of the ball
+     * @param b = ball
+     * @param p = paddle last hit the ball
+     */
     void usePU(Ball b, Paddle p);
 }
